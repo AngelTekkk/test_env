6fe3473c97f7a3625e9459eec04a35078969552e
Andriy Makarov <angeltek69@gmail.com>
2022-09-14 20:33:39 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 export const App = () => {
   const { REACT_APP_VERSION, NODE_ENV } = process.env;
+  console.log(NODE_ENV);
+  console.log(REACT_APP_VERSION);
   console.log(
     NODE_ENV === 'production' ? window.REACT_APP_VERSION : REACT_APP_VERSION
   );
